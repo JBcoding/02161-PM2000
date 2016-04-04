89a5056cda0a334d32c520d504b8867f7cf3bced
JBcoding <mads12bjoern@gmail.com>
2016-04-04 15:56:40 +0200

    init setup done

--- PM2000/test/PM2000Test.java
+++ PM2000/test/PM2000Test.java
@@ -4,7 +4,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class PM2000_test {
+public class PM2000Test {
     @Test
     public void TestConstant_Constructor_SetsValue() {
         PM2000 pm2000 = new PM2000();
---
+++ PM2000/test/ProjectTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by madsbjoern on 04/04/16.
+ */
+public class ProjectTest {
+}
