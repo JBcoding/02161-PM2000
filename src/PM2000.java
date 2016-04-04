8369f3b6e075f79a9c80e17e23b739dc1530cfd4
JBcoding <mads12bjoern@gmail.com>
2016-04-04 15:31:56 +0200

    init

---
+++ src/PM2000.java
@@ -0,0 +1,5 @@
+public class PM2000 {
+    public PM2000 {
+
+    }
+}
---
+++ test/PM2000_test.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PM2000_test {
+    @Test
+    public void TestDelay_Microseconds_ReturnsInMicroseconds() {
+        PM2000 pm2000 = new PM2000();
+    }
+}
