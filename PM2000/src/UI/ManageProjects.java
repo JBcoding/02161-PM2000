0a1a89444e294c8b516ebf8ec6c924341bda4d3e
magnus0512 <magnusdal10@hotmail.com>
2016-05-03 16:05:33 +0200

    More functionality to Manage Projects

--- PM2000/src/UI/ManageProjects.java
+++ PM2000/src/UI/ManageProjects.java
@@ -11,6 +11,10 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
+
 /**
  * Created by madsbjoern on 02/05/16.
  */
@@ -23,7 +27,7 @@ public class ManageProjects extends Stage {
     protected ListView<Project> projectList;
     protected ComboBox<User> projectLeadPicker;
     protected ListView<Project> userInProject, userNotInProject;
-    protected Project currentProject;
+    protected DatePicker startDatePicker, endDatePicker;
 
     public ManageProjects() {
         root = new AnchorPane();
@@ -39,6 +43,9 @@ public class ManageProjects extends Stage {
         projectList = new ListView<>();
         projectList.getItems().addAll(PM2000.getProjects());
         projectList.setOnKeyPressed(new EventHandler<KeyEvent>() {@Override public void handle(KeyEvent ke) {if ((ke.getCode() == KeyCode.ESCAPE)) {close();}}});
+        if (projectList.getItems().size() > 0) {
+            projectList.getSelectionModel().clearAndSelect(0);
+        }
         projectList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Project>() {
             @Override
             public void changed(ObservableValue<? extends Project> observable, Project oldValue, Project newValue) {
@@ -58,6 +65,13 @@ public class ManageProjects extends Stage {
         projectEndDateLabel.setPrefWidth(280);
 
         updateLabels();
+        endDatePicker = new DatePicker();
+        endDatePicker.setPrefWidth(225);
+        endDatePicker.setShowWeekNumbers(true);
+
+        startDatePicker = new DatePicker();
+        startDatePicker.setPrefWidth(225);
+        startDatePicker.setShowWeekNumbers(true);
 
         projectLeadPicker = new ComboBox<>();
         projectLeadPicker.getItems().addAll(PM2000.getUsers());
@@ -65,20 +79,32 @@ public class ManageProjects extends Stage {
         projectLeadPicker.setPrefWidth(225);
 
         setProjectLeadButton = new Button("Set project lead");
+        setProjectLeadButton.setOnAction(new EventHandler<ActionEvent>() {@Override public void handle(ActionEvent event) {setSetProjectLead();}});
+        setProjectLeadButton.setOnKeyPressed(new EventHandler<KeyEvent>() {@Override public void handle(KeyEvent ke) {if (ke.getCode().equals(KeyCode.ENTER)) {setSetProjectLead();}}});
+
         setProjectStartDateButton = new Button("Set start date");
+        setProjectStartDateButton.setOnAction(new EventHandler<ActionEvent>() {@Override public void handle(ActionEvent event) {setStartDate();}});
+        setProjectStartDateButton.setOnKeyPressed(new EventHandler<KeyEvent>() {@Override public void handle(KeyEvent ke) {if (ke.getCode().equals(KeyCode.ENTER)) {setStartDate();}}});
+
         setProjectEndDateButton = new Button("Set end date");
+        setProjectEndDateButton.setOnAction(new EventHandler<ActionEvent>() {@Override public void handle(ActionEvent event) {setEndDate();}});
+        setProjectEndDateButton.setOnKeyPressed(new EventHandler<KeyEvent>() {@Override public void handle(KeyEvent ke) {if (ke.getCode().equals(KeyCode.ENTER)) {setEndDate();}}});
+
         deleteProjectButton = new Button("Delete project");
         deleteProjectButton.setOnAction(new EventHandler<ActionEvent>() {@Override public void handle(ActionEvent event) {deleteProject();}});
         deleteProjectButton.setOnKeyPressed(new EventHandler<KeyEvent>() {@Override public void handle(KeyEvent ke) {if (ke.getCode().equals(KeyCode.ENTER)) {deleteProject();}}});
+
         toggleUserStatus = new Button("Toggle user status");
 
         setProjectLeadButton.setPrefWidth(150);
-        setProjectStartDateButton.setPrefWidth(375);
-        setProjectEndDateButton.setPrefWidth(375);
+        setProjectStartDateButton.setPrefWidth(150);
+        setProjectEndDateButton.setPrefWidth(150);
         toggleUserStatus.setPrefWidth(660);
         deleteProjectButton.setPrefWidth(660);
 
         userInProject = new ListView<>();
+
+
         userNotInProject = new ListView<>();
 
         userInProject.setPrefWidth(333);
@@ -94,8 +120,8 @@ public class ManageProjects extends Stage {
         HBox tempBox6 = new HBox();
 
         tempBox1.getChildren().addAll(projectLeadLabel, projectLeadPicker, setProjectLeadButton);
-        tempBox2.getChildren().addAll(projectStartDateLabel, setProjectStartDateButton);
-        tempBox3.getChildren().addAll(projectEndDateLabel, setProjectEndDateButton);
+        tempBox2.getChildren().addAll(projectStartDateLabel, startDatePicker, setProjectStartDateButton);
+        tempBox3.getChildren().addAll(projectEndDateLabel, endDatePicker, setProjectEndDateButton);
         tempBox4.getChildren().addAll(userInProject, userNotInProject);
         tempBox5.getChildren().add(toggleUserStatus);
         tempBox6.getChildren().add(deleteProjectButton);
@@ -117,10 +143,12 @@ public class ManageProjects extends Stage {
     }
 
     private void updateLabels() {
+        Project currentProject = projectList.getSelectionModel().getSelectedItem();
         projectLeadLabel.setText("Project leader: " + ((currentProject == null || currentProject.getProjectLead() == null) ? "nobody" : currentProject.getProjectLead()));
-        projectStartDateLabel.setText("Start date: " + ((currentProject == null || currentProject.getStartDate() == null) ? "no date set yet" : currentProject.getStartDate()));
-        projectEndDateLabel.setText("End date: " + ((currentProject == null || currentProject.getEndDate() == null) ? "no date set yet" : currentProject.getEndDate()));
+        projectStartDateLabel.setText("Start date: " + ((currentProject == null || currentProject.getStartDate() == null) ? "no date set yet" : (currentProject.getStartDate().getDate() + "-" + (currentProject.getStartDate().getMonth() + 1) + "-" + (currentProject.getStartDate().getYear() + 1900))));
+        projectEndDateLabel.setText("End date: " + ((currentProject == null || currentProject.getEndDate() == null) ? "no date set yet" : (currentProject.getEndDate().getDate() + "-" + (currentProject.getEndDate().getMonth() + 1) + "-" + (currentProject.getEndDate().getYear() + 1900))));
     }
+
     public void deleteProject() {
         Project project = projectList.getSelectionModel().getSelectedItem();
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + project + " ?", ButtonType.YES, ButtonType.NO);
@@ -132,4 +160,41 @@ public class ManageProjects extends Stage {
             projectList.getItems().addAll(PM2000.getProjects());
         }
     }
+    public void setSetProjectLead() {
+        User user = projectLeadPicker.getSelectionModel().getSelectedItem();
+        Project project = projectList.getSelectionModel().getSelectedItem();
+        project.setProjectLead(user);
+        updateLabels();
+    }
+    public void setStartDate() {
+        if (startDatePicker.getValue() == null){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "No date selected", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+        Date date = Date.from(Instant.from(startDatePicker.getValue().atStartOfDay(ZoneId.systemDefault())));
+        Project project = projectList.getSelectionModel().getSelectedItem();
+        if(project.setStartDate(date)){
+            updateLabels();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Start date invalid", ButtonType.OK);
+            alert.showAndWait();
+        }
+    }
+    public void setEndDate() {
+        if (endDatePicker.getValue() == null){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "No date selected", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+        Date date = Date.from(Instant.from(endDatePicker.getValue().atStartOfDay(ZoneId.systemDefault())));
+        Project project = projectList.getSelectionModel().getSelectedItem();
+        if(project.setEndDate(date)){
+            updateLabels();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR, "End date invalid", ButtonType.OK);
+            alert.showAndWait();
+        }
+    }
 }
+
